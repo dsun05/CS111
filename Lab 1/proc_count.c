03070a6f85a48acbd9e9d28e2527345a6f5a0479
dsun05 <dsun05@g.ucla.edu>
2025-07-04 23:10:04 -0700

    Update lab 1

--- Lab 1/proc_count.c
+++ Lab 1/proc_count.c
@@ -1,32 +1,29 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/proc_fs.h>
-#include <linux/seq_file.h>
 #include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <linux/seq_file.h>
 
 static struct proc_dir_entry *entry;
 
-static int proc_count(struct seq_file *m, void *v){
-	int len=0;
-	struct task_struct *task_list;
-	for_each_process(task_list){
-		len++;
-	}
-	seq_printf(m, "%d\n", len);
-	return 0;
+static int proc_count(struct seq_file *m, void *v) {
+  int count = 0;
+  struct task_struct *task;
+  for_each_process(task) { count++; }
+  seq_printf(m, "%d\n", count);
+  return 0;
 }
 
-static int __init proc_count_init(void)
-{
-	entry = proc_create_single("count", 0, NULL, proc_count);
-	pr_info("proc_count: init\n");
-	return 0;
+static int __init proc_count_init(void) {
+  entry = proc_create_single("count", 0, NULL, proc_count);
+  pr_info("proc_count: init\n");
+  return 0;
 }
 
-static void __exit proc_count_exit(void)
-{
-	proc_remove(entry);
-	pr_info("proc_count: exit\n");
+static void __exit proc_count_exit(void) {
+  proc_remove(entry);
+  pr_info("proc_count: exit\n");
 }
 
 module_init(proc_count_init);
